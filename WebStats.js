95534405c61d21d1d9bc4caf6993ef3ed669c5eb
Peter Batenburg <peter@deejays.nl>
2026-05-26 22:24:48 +0200

    Bump version to 2.1.0

--- WebStats.js
+++ WebStats.js
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////
 ///                                                         ///
-///  WEBSTATS PLUGIN FOR FM-DX-WEBSERVER (V2.0.1)          ///
+///  WEBSTATS PLUGIN FOR FM-DX-WEBSERVER (V2.1.0)          ///
 ///                                                         ///
 ///  Visitor statistics from serverlog.txt                   ///
 ///                                                         ///
@@ -14,7 +14,7 @@ const https = require('https');
 // Plugin configuration
 var pluginConfig = {
     name: 'WebStats',
-    version: '2.0.1',
+    version: '2.1.0',
     author: 'petertjeee',
     frontEndPath: 'WebStats/webstats-plugin.js'
 };
